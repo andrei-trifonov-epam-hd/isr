67589ed2927bd94ef7016235ac58af9b062a62cc
Andrei Trifonov <andrei.trifonov@harley-davidson.com>
2024-01-08 23:20:12 -0500

    Links for a page refresh

--- packages/ui/src/Navigation.tsx
+++ packages/ui/src/Navigation.tsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
 
 const Navigation = () => (
-    <nav>
-      <ul>
-        <li>
-          <Link href={"/app-router"}>App Router</Link>
-        </li>
-        <li>
-          <Link href={"/pages-router"}>Pages Router</Link>
-        </li>
-      </ul>
-    </nav>
+  <nav>
+    <ul>
+      <li>
+        <Link href={"/app-router"}>App Router (client router)</Link>
+      </li>
+      <li>
+        <a href={"/app-router"}>App Router (roundtrip)</a>
+      </li>
+      <li>
+        <Link href={"/pages-router"}>Pages Router (client router)</Link>
+      </li>
+      <li>
+        <a href={"/pages-router"}>Pages Router (roundtrip)</a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navigation;
